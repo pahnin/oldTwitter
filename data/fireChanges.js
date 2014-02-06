709679bcfbacf8e5da129a9a78308ede0d7114ee
pahnin <pahninsd@gmail.com>
2014-02-06 15:10:33 +0530

    First commit of oldTwitter add on

---
+++ data/fireChanges.js
@@ -0,0 +1,5 @@
+$(".global-nav .container").css({"max-width": "900px"}); 
+$(".global-nav-inner").css({"background": "#3d3d3d"}); 
+$(".nav > li > a, .nav > li > button").css({ "color": "rgb(187, 187, 187)"}); 
+$("#message-drawer .message").css({ "background": "none repeat scroll 0% 0% rgb(255, 255, 255)", "padding": "6px" }); 
+$(".stats a strong").css({"font-size":"14px"});
